5f53ba6aa9948e0957e39367453ece6776237faa
unknown <Noelia Neyar Vsquez Crespo>
2025-09-01 22:20:39 -0400

    feat: modificando status login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import { comparar } from '../common/bcrypt.js';
 import config from '../config/env.js';
-import { Status } from '../constants/index.js';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 
@@ -12,7 +11,7 @@ async function login(req, res, next) {
         .status(400)
         .json({ message: 'Username or password is required' });
 
-    const user = await User.findOne({ where: { username, status:Status.ACTIVE } });
+    const user = await User.findOne({ where: { username } });
 
     if (!user) return res.status(403).json({ message: 'User not found' });
 
